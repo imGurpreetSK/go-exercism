49e0da7f4486fa920173a6d1d87f177f39bb5b8d
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-03-03 13:07:46 -0800

    Improve solution for two_fer.

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,9 +1,10 @@
 package twofer
 
+import "fmt"
+
 func ShareWith(name string) string {
 	if name == "" {
 		return "One for you, one for me."
 	}
-
-	return "One for " + name + ", one for me."
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
