469d54658bb14578f5a732803492b75a8e14e928
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-03-06 18:12:35 -0800

    Solve vehicle-purchase.go

---
+++ vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,30 @@
+package purchase
+
+import "fmt"
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	return kind == "car" || kind == "truck"
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	var result string
+	if option1 > option2 {
+		result = option2
+	} else {
+		result = option1
+	}
+	return fmt.Sprintf("%s is clearly the better choice.", result)
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 {
+		return 0.8 * originalPrice
+	} else if age >= 10 {
+		return 0.5 * originalPrice
+	} else {
+		return 0.7 * originalPrice
+	}
+}
