99f70e1d56a447ec08936420c269edeea2e7f584
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-03-19 15:32:19 -0700

    Improve gross-store.

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -2,16 +2,14 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	units := map[string]int{}
-
-	units["quarter_of_a_dozen"] = 3
-	units["half_of_a_dozen"] = 6
-	units["dozen"] = 12
-	units["small_gross"] = 120
-	units["gross"] = 144
-	units["great_gross"] = 1728
-
-	return units
+	return map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
 }
 
 // NewBill creates a new bill.
@@ -26,7 +24,7 @@ func AddItem(bill, units map[string]int, item, unit string) bool {
 		return false
 	}
 
-	bill[item] = bill[item] + count
+	bill[item] += count
 
 	return true
 }
@@ -34,16 +32,8 @@ func AddItem(bill, units map[string]int, item, unit string) bool {
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
 	itemInBill, itemExists := bill[item]
-	if !itemExists {
-		return false
-	}
-
 	value, valueExists := units[unit]
-	if !valueExists {
-		return false
-	}
-
-	if itemInBill-value < 0 {
+	if !itemExists || !valueExists || itemInBill-value < 0 {
 		return false
 	}
 
@@ -59,9 +49,5 @@ func RemoveItem(bill, units map[string]int, item, unit string) bool {
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
 	itemCountInBill, itemExists := bill[item]
-	if !itemExists {
-		return 0, false
-	}
-
-	return itemCountInBill, true
+	return itemCountInBill, itemExists
 }
